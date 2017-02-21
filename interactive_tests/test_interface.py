04ae0bacb8ad830ee610b7342ee186748c63d0f2
Fake-Name <something@fake-url.com>
2017-02-20 21:44:51 -0800

    More debugging stuff.

--- interactive_tests/test_interface.py
+++ interactive_tests/test_interface.py
@@ -21,6 +21,24 @@ import scanner.uiFrontend
 import scanner.hashFile as hasher
 import UniversalArchiveInterface as uar
 
+def hash_print(phash):
+
+	bits = [1 if 1 << x & phash else 0 for x in range(63, -1, -1)]
+	for x in range(0, 64, 8):
+		print(bits[x:x+8])
+
+
+def hash_comp(phash1, phash2):
+
+	bits1 = [1 if 1 << x & phash1 else 0 for x in range(63, -1, -1)]
+	bits2 = [1 if 1 << x & phash2 else 0 for x in range(63, -1, -1)]
+	distance = len([1 for b in range(64) if bits1[b] != bits2[b]])
+	print("Distance: ", distance)
+	for x in range(0, 64, 8):
+		diff = [[1 if bits1[b] != bits2[b] else 0 for b in range(7+x, -1+x, -1)]]
+		print(bits1[x:x+8], bits2[x:x+8], diff)
+
+
 def rawHashFile(archPath):
 	archIterator = uar.ArchiveReader(archPath)
 
@@ -58,20 +76,23 @@ def doListDupes(on_file):
 	remote = rpyc.connect("localhost", 12345)
 	commons = remote.root.listDupes(filePath=on_file)
 	print("result:")
-	pprint.pprint(commons)
+	print(commons)
 	print("Wut?")
 
 def doSingleSearch(phash):
 	print("Search for: ", phash)
 	phash = int(phash)
+	hash_print(phash)
 	print(phash)
 
 	print("Finding files similar to: '{}'".format(phash))
 	remote = rpyc.connect("localhost", 12345)
 	commons = remote.root.single_phash_search(phash=phash)
 
-	print("Common:")
-	print(commons)
+	print("Common files:")
+	for item in commons:
+		print(item[1].ljust(100), item[0])
+		hash_comp(phash, item[4])
 
 def go(scanConf):
 	if scanConf.testScan:
